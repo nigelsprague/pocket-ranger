aab74891bb4a6399a88134160aeafabefd49d8fb
Matthew Marchal <matthew.marchal@gmail.com>
2024-10-02 13:35:51 -0600

    Basic info on about page

--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -3,8 +3,50 @@
 </script>
 
 <template>
-  <div>
-
+  <!-- + Readme/About Page of app problem statement, concept, description, contributors, and languages/tools used -->
+  <div class="container">
+    <div>
+      <h1>About Pocket Ranger</h1>
+      <p>For Pocket Ranger, we looked at the existing process of researching and visiting national parks and decided
+        that we wanted to improve and streamline the experience by making an app that has all the needed information in
+        one spot and allows the user to keep up-to-date on alerts and events in selected national parks.</p>
+    </div>
+    <div>
+      <h2>Concepts and Tools Used</h2>
+      <p>To do this, we wanted to retrieve and display the relevant data from the National Parks Service API using HTML,
+        CSS, and JS while utilizing the useful frameworks of Bootstrap, Mongoose, ExpressJs, and Vue.</p>
+      <p>We also display the relevant locations using the HERE Maps API.</p>
+    </div>
+    <div>
+      <h2>Contributors</h2>
+      <div class="d-flex">
+        <div>
+          <img src="" alt="">
+        </div>
+        <div>
+          <h4>Autumn Page</h4>
+          <p>Bio here</p>
+        </div>
+      </div>
+      <div class="d-flex">
+        <div>
+          <img src="" alt="">
+        </div>
+        <div>
+          <h4>Matthew Marchal</h4>
+          <p>Bio here</p>
+        </div>
+      </div>
+      <div class="d-flex">
+        <div>
+          <img src="" alt="">
+        </div>
+        <div>
+          <h4>Nigel Sprague</h4>
+          <p>Bio here</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 

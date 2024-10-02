74097d788d45e1e2943e17e85e3e5c87d2531fc7
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-02 13:33:34 -0600

    Storing parks in AppState, created ParkCard and added to HomePage

---
+++ client/src/components/globals/ParkCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Park } from '@/models/Park.js';
+
+defineProps({ park: {type: Park, required: true}})
+
+</script>
+
+
+<template>
+<div class="card">
+  <img src="..." alt="..." class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title">{{ park.fullName }}</h5>
+    <!-- <p class="card-text">{{ park.description }}</p> -->
+  </div>
+</div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,9 +1,12 @@
 <script setup>
+import { AppState } from '@/AppState.js';
+import ParkCard from '@/components/globals/ParkCard.vue';
 import { parksService } from '@/services/ParksService.js';
 import { logger } from '@/utils/Logger.js';
 import Pop from '@/utils/Pop.js';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 
+const parks = computed(() => AppState.parks)
 
 onMounted(() => {
   getAllParks()
@@ -21,8 +24,17 @@ async function getAllParks(){
 </script>
 
 <template>
-  <div>
-    <!-- <HereMap :center="center" /> -->
+  <div class="container">
+    <section class="row">
+      <div class="col-12">
+        <h1>All Parks</h1>
+      </div>
+    </section>
+    <section class="row">
+      <div v-for="park in parks" :key="park.id" class="col-md-4">
+        <ParkCard :park="park"/>
+      </div>
+    </section>
   </div>
 </template>
 

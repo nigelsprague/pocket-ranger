51a9d0cc38b6ddb19c50a770c442335a20933712
Nigel Sprague <spraguenigel27@gmail.com>
2024-10-02 10:24:03 -0600

    Park Model and Account Schema

---
+++ client/src/models/Park.js
@@ -0,0 +1,17 @@
+export class Park {
+  constructor(data) {
+    this.id = data.id
+    this.fullName = data.fullName
+    this.description = data.description
+    this.images = data.images
+    this.parkCode = data.parkCode
+    this.entranceFees = data.entranceFees
+    this.operatingHours = data.operatingHours
+    this.weather = data.weather
+    this.states = data.states
+    this.designation = data.designation
+    this.contacts = data.contacts
+    this.activities = data.activities
+    this.latLong = data.latLong
+  }
+}
--- server/src/models/Account.js
+++ server/src/models/Account.js
@@ -6,8 +6,9 @@ export const AccountSchema = new Schema(
     subs: [{ type: String }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String }
+    picture: { type: String },
     // NOTE If you wish to add additional properties do so here
+    phone: { type: Number, unique: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )

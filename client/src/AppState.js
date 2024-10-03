38b51e2860bc830bf9677cc22403ca668e74f4ab
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-03 10:54:02 -0600

    Added things to do in the AppState

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -11,7 +11,8 @@ export const AppState = reactive({
   parkQuery: '',
   /**@type {import('./models/Park.js').Park} */
   activePark: null,
-
+  /**@type {import('./models/ThingsToDo.js').ThingsToDo[]} */
+  thingsToDo: [],
   parkList: 'acad,arch,badl,bibe,bisc,blca,brca,cany,care,cave,chis,cong,crla,cuva,dena,deva,drto,ever,jeff,gaar,glac,glba,grca,grsa,grte,grba,grsm,gumo,hale,havo,hosp,indu,isro,jotr,katm,kefj,kova,lacl,lavo,maca,meve,mora,neri,noca,npsa,olym,pefo,pinn,redw,romo,sagu,seki,shen,thro,viis,voya,whsa,wica,wrst,yell,yose,zion'
 })
 

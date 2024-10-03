eaa8fdcce7f2753823c58c88a0c7d3329440a3b8
Matthew Marchal <matthew.marchal@gmail.com>
2024-10-03 10:16:44 -0600

    Created follower controller/service and added functionality to create followers

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ParkSchema } from '../models/Park';
+import { FollowerSchema } from '../models/Follower.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
   Parks = mongoose.model('Park', ParkSchema);
+  Followers = mongoose.model('Follower', FollowerSchema);
 }
 
 export const dbContext = new DbContext()

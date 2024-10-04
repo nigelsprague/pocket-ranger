f52f603ebb532b7acd57e718af63c56a65a1090f
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-04 11:23:28 -0600

    Created fee card and fee model

---
+++ client/src/components/globals/FeeCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Fee } from '@/models/Fee.js';
+
+defineProps({ activeFee: { type: Fee, required: true } })
+
+</script>
+
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="card-title">{{ activeFee.title }}</div>
+      <h1>${{ activeFee.cost }}</h1>
+      <p class="card-text">{{ activeFee.description }}</p>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped></style>
---
+++ client/src/models/Fee.js
@@ -0,0 +1,8 @@
+export class Fee {
+  constructor(data) {
+    this.id = data.id
+    this.cost = data.cost
+    this.title = data.title
+    this.description = data.description
+  }
+}

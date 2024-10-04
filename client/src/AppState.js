3b07f7f758f76434f8648ee64cd63512f5209254
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-04 12:00:47 -0600

    Storing alerts to the app state

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -15,6 +15,8 @@ export const AppState = reactive({
   activePark: null,
   /**@type {import('./models/ThingsToDo.js').ThingsToDo[]} */
   thingsToDo: [],
+  /**@type {import('./models/Alert.js').Alert[]} */
+  alerts: [],
   parkList: 'acad,arch,badl,bibe,bisc,blca,brca,cany,care,cave,chis,cong,crla,cuva,dena,deva,drto,ever,jeff,gaar,glac,glba,grca,grsa,grte,grba,grsm,gumo,hale,havo,hosp,indu,isro,jotr,katm,kefj,kova,lacl,lavo,maca,meve,mora,neri,noca,npsa,olym,pefo,pinn,redw,romo,sagu,seki,shen,thro,viis,voya,whsa,wica,wrst,yell,yose,zion'
 })
 
--- client/src/services/AlertsService.js
+++ client/src/services/AlertsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { npsAPI } from "./AxiosService.js"
+import { Alert } from "@/models/Alert.js"
+import { AppState } from "@/AppState.js"
 
 class AlertsService {
   async getAlertByCode(parkCode) {
     const response = await npsAPI.get(`/alerts/?parkcode=${parkCode}`)
     logger.log('Got alerts - alert service', response.data.data)
+    const newAlert = response.data.data.map(alertData => new Alert(alertData))
+    AppState.alerts = newAlert
   }
 
 }

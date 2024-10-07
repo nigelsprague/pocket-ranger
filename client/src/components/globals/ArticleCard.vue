7d28767e2d5c70228e1b395fbf8eee6ba30d660b
Nigel Sprague <spraguenigel27@gmail.com>
2024-10-07 10:28:15 -0600

    article feed for  followed parks on account page

--- client/src/components/globals/ArticleCard.vue
+++ client/src/components/globals/ArticleCard.vue
@@ -12,14 +12,14 @@ defineProps({ article: { type: Article, required: true } })
       <div class="card p-3 my-1">
         <div class="d-flex">
           <div class="col-4">
-            <img class="img-fluid" :src="article.listingImage.url" :alt="article.listingImage.altText">
-            <i  v-if="!article.listingImage.url">No Article Image</i>
+            <i v-if="!article.listingImage.url">No Article Image</i>
+            <img v-else class="img-fluid" :src="article.listingImage.url" :alt="article.listingImage.altText">
           </div>
           <div class="col-8">
             <div class="px-3">
               <h5>{{ article.title }}</h5>
               <p class="m-0">{{ article.body }}</p>
-              <br/>
+              <br />
               <a v-if="article.url" :href="article.url" class="btn bg-info">Read More</a>
             </div>
           </div>
@@ -42,5 +42,12 @@ img {
   background-color: #FDFBF1;
   border-color: #2C4A1E;
   border-width: 4px;
+}
+
+p {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 4;
+  overflow: hidden;
 }
 </style>
--- client/src/services/ArticlesService.js
+++ client/src/services/ArticlesService.js
@@ -4,6 +4,13 @@ import { Article } from "@/models/Article.js"
 import { AppState } from "@/AppState.js"
 
 class ArticlesService {
+  async getArticleByFavorites(favoritedParks) {
+    const limit = 10
+    const response = await npsAPI.get(`/articles/?limit=${limit}&parkcode=${favoritedParks}`)
+    logger.log('Got articles - article service', response.data.data)
+    const newArticle = response.data.data.map(articleData => new Article(articleData))
+    AppState.articles = newArticle
+  }
   async getArticleByCode(parkCode) {
     const response = await npsAPI.get(`/articles/?parkcode=${parkCode}`)
     logger.log('Got articles - article service', response.data.data)

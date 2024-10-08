defbd070b5b3e3ace42cb0b4c9ce75d84125f7b8
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-08 17:16:49 -0600

    Got pagination on articles service

--- client/src/services/ArticlesService.js
+++ client/src/services/ArticlesService.js
@@ -2,30 +2,35 @@ import { logger } from "@/utils/Logger.js"
 import { npsAPI } from "./AxiosService.js"
 import { Article } from "@/models/Article.js"
 import { AppState } from "@/AppState.js"
+import { pagesService } from "./PagesService.js"
 
 class ArticlesService {
-  // async getArticleByFavorites(favoritedParks) {
-  //   const limit = 10
-  //   const response = await npsAPI.get(`/articles/?limit=${limit}&parkcode=${favoritedParks}`)
-  //   logger.log('Got articles - article service', response.data.data)
-  //   const newArticle = response.data.data.map(articleData => new Article(articleData))
-  //   AppState.articles = newArticle.reverse()
-  // }
-  async getArticleByCode(parkCodes) {
-    const responseForTotal = await npsAPI.get(`/articles/?parkcode=${parkCodes}&limit=1`);
-    const total = parseInt(responseForTotal.data.total);
 
-    // TODO Pagination
-    const page = 1;
-    const limit = 10;
-    let start = total - (limit * page);
-    if (start < 1) {
-      start = 0;
-    }
-    const response = await npsAPI.get(`/articles/?parkcode=${parkCodes}&start=${start}&limit=${limit}`)
-    logger.log('Got articles - article service', response.data.data)
+  limit = 10
+  async changeArticlesPage(pageNumber, parkCode) {
+    const response = await npsAPI.get(`/articles/?parkcode=${parkCode}&start=${10 * (pageNumber - 1)}&limit=${this.limit}`)
+    logger.log('Changed articles page - article service', response.data)
     const newArticle = response.data.data.map(articleData => new Article(articleData))
     AppState.articles = newArticle.reverse()
+    pagesService.savePages(response.data, this.limit)
+  }
+
+  async getArticleByCode(parkCodes) {
+    // const responseForTotal = await npsAPI.get(`/articles/?parkcode=${parkCodes}&limit=1`);
+    // const total = parseInt(responseForTotal.data.total);
+
+    // // TODO Pagination
+    // const page = 1;
+    // const limit = 10;
+    // let start = total - (limit * page);
+    // if (start < 1) {
+    //   start = 0;
+    // }
+    const response = await npsAPI.get(`/articles/?parkcode=${parkCodes}&start=0&limit=${this.limit}`)
+    logger.log('Got articles - article service', response.data)
+    const newArticle = response.data.data.map(articleData => new Article(articleData))
+    AppState.articles = newArticle.reverse()
+    pagesService.savePages(response.data, this.limit)
   }
 
 }

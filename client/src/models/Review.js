69f20a5f521b8d0ce24b55b13964e62ed4dbf61a
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-09 15:02:40 -0600

    Edited review model and saved reviews to the appstate

--- client/src/models/Review.js
+++ client/src/models/Review.js
@@ -1,10 +1,13 @@
+import { Account } from "./Account.js"
+
 export class Review {
   constructor(data) {
+    this.id = data.id
     this.recommended = data.recommended
     this.title = data.title
     this.body = data.body
     this.creatorId = data.creatorId
-    this.creator = data.creator
+    this.creator = new Account(data.creator)
     this.parkId = data.parkId
     this.park = data.park
   }

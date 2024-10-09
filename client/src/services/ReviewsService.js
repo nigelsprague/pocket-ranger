7633f246eaf944d74403f3a76c675333d0264796
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-09 16:31:05 -0600

    Delete review on backend working

--- client/src/services/ReviewsService.js
+++ client/src/services/ReviewsService.js
@@ -1,12 +1,14 @@
 import { api } from "./AxiosService.js"
 import { Review } from "@/models/Review.js"
 import { AppState } from "@/AppState.js"
+import { logger } from "@/utils/Logger.js"
 
 class ReviewsService {
   async createReview(reviewData) {
     const response = await api.post(`api/reviews`, reviewData)
     const createReview = new Review(response.data)
     AppState.reviews.push(createReview)
+    logger.log(createReview)
     return createReview
   }
 

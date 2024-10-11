24b407b8cd823ed8a9479f610de17cfd1f1ec898
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-11 09:49:45 -0600

    depoly

---
+++ server/src/controllers/BookmarksController.js
@@ -0,0 +1,35 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import BaseController from "../utils/BaseController.js";
+import { bookmarksService } from "../services/BookmarksService.js";
+
+export class ArticlesController extends BaseController {
+  constructor() {
+    super('api/bookmarks')
+    this.router
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createBookmark)
+      .delete('/:bookmarkId', this.deleteBookmark)
+  }
+
+  async createBookmark(request, response, next) {
+    try {
+      const bookmarkData = request.body
+      bookmarkData.creatorId = request.userInfo.id
+      const bookmark = await bookmarksService.createBookmark(bookmarkData)
+      response.send(bookmark)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteBookmark(request, response, next) {
+    try {
+      const bookmarkId = request.params.bookmarkId
+      const userId = request.userInfo.id
+      const message = await bookmarksService.deleteBookmark(bookmarkId, userId)
+      response.send(message)
+    } catch (error) {
+      next(error)
+    }
+  }
+}

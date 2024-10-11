b292f1ec0747d9e340f9031dc4d51af2ad8ebba8
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-11 17:24:45 -0600

    Removed article from backend

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -5,13 +5,11 @@ import { FollowerSchema } from '../models/Follower.js';
 import { PostSchema } from '../models/Post.js';
 import { ReviewSchema } from '../models/Review.js';
 import { BookmarksSchema } from '../models/Bookmark.js';
-import { ArticleSchema } from '../models/Article.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
   Parks = mongoose.model('Park', ParkSchema);
   Bookmarks = mongoose.model('Bookmark', BookmarksSchema)
-  Articles = mongoose.model('Article', ArticleSchema)
   Followers = mongoose.model('Follower', FollowerSchema);
   Posts = mongoose.model('Post', PostSchema);
   Reviews = mongoose.model('Review', ReviewSchema);
--- server/src/models/Article.js
+++
@@ -1,9 +0,0 @@
-import { Schema } from "mongoose";
-
-export const ArticleSchema = new Schema(
-  {
-    title: { type: String, required: true },
-    listingDescription: { type: String, required: true }
-  },
-  { timestamps: true, toJSON: { virtuals: true } }
-)

dd7e200a0d618f7ca638ad25525ba09696b5d225
Nigel Sprague <spraguenigel27@gmail.com>
2024-10-11 17:53:54 -0600

    weather styling ugly sorry i will fix

--- client/src/components/globals/Weather.vue
+++ client/src/components/globals/Weather.vue
@@ -10,14 +10,29 @@ defineProps({
 
 
 <template>
-  <div class="container">
-    <img :src="weather.iconUrl" alt="weather icon">
-    <span>{{ weather.tempCels.toFixed(2) }}° C</span>
-    <span>{{ weather.tempFar.toFixed(2) }}° F</span>
-    <span>{{ weather.weather }}</span>
-    <span>{{ weather.wind }} mph</span>
+  <div class="card bg-weather">
+    <div class="container">
+      <section class="row align-items-center">
+        <div class="col">
+          <div class="d-flex flex-column text-center align-items-center">
+            <img :src="weather.iconUrl" alt="weather icon">
+            <h4>{{ weather.weather }}</h4>
+          </div>
+        </div>
+        <div class="col">
+          <h6>{{ weather.tempCels.toFixed(2) }}° C</h6>
+          <h6>{{ weather.tempFar.toFixed(2) }}° F</h6>
+          <h6>{{ weather.wind }} mph Winds</h6>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bg-weather {
+  background-color: var(--bs-weather);
+  color: var(--bs-text);
+}
+</style>
